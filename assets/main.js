120b98d042b261a30e582bc310be3690a127e8fc
Paula Fleck <paulakfleck@gmail.com>
2021-03-13 15:23:10 -0300

    OperatorMono

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,16 @@
 const readMore = () => {
     let $readMore = [...document.querySelectorAll('.read-more-btn')];
 
-//     $readMore.forEach($btn => {
-//         let $article = $btn.previousSibling;
+    $readMore.forEach($btn => {
+        let $article = $btn.previousSibling;
 
-//         $btn.addEventListener('click', (e) => {
-//             e.preventDefault();
+        $btn.addEventListener('click', (e) => {
+            e.preventDefault();
             
-//             $article.classList.add('open');
-//             $btn.parentNode.removeChild($btn);
-//         });
-//     });
+            $article.classList.add('open');
+            $btn.parentNode.removeChild($btn);
+        });
+    });
 }
 
 const init = () => {
